8a3ef16689451c724847ddb08bc39088be14607a
6peng <hi6peng@qq.com>
2023-09-20 19:33:12 +0800

    hello dkms

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init hello_init(void)
+{
+        pr_info("Hello world.\n");
+        return 0;
+}
+
+static void __exit hello_exit(void)
+{
+        pr_info("Goodbye world.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
